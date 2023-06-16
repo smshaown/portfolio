a5f8a293e0d13b41057a58d1592b680a57ee6acf
smshaown <smshaown65@gmail.com>
2023-06-16 23:18:17 +0600

    pages and comoents added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './App.css'
 
+import Navbar from './components/Navbar'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Portfolio from './pages/Portfolio/Portfolio'
+import Contact from './pages/Contact/Contact'
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
