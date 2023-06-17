b48713f3f4371d2b564f4e120e0c7ff91f9ccf81
smshaown <smshaown65@gmail.com>
2023-06-17 08:55:10 +0600

    index.css style class add

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
      <BrowserRouter>
      <Navbar />
        <Routes>
-        <Route index element={<Home />}>
+        <Route path='/' element={<Home />}>
          <Route path='about' element={<About />}></Route>
          <Route path='portfolio' element={<Portfolio />}></Route>
          <Route path='contact' element={<Contact />}></Route>
