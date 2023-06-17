2fbab3b25a409086d8bebe12f2e6b66dc82e55aa
smshaown <smshaown65@gmail.com>
2023-06-17 13:25:57 +0600

    home page create now

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import Profile  from '../../assets/profile.png'
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+import './Home.css'
+
+
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <section className='home section grid'>
+            <div>
+              <img src={Profile} alt="" className='home__img' />
+            </div>
+
+            <div className='home__content'>
+                <div className='home_data'>
+                    <h1 className='home-title'>
+                        <span>I'm S M Shaown</span> Web Designer
+                    </h1>
+                    <p>I'm a Tunisian based web designer & front-end developer focused on crafting clean $ user-friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+                </div>
+                <Link to="/about" className='button'>
+                    More About Me <span className='button_icon'><FaArrowRight /></span>
+                </Link>
+            </div>
+        </section>
     );
 };
 
