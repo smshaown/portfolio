3d904c58d026f5dc187727a8e92cd7a3ac25c11d
smshaown <smshaown65@gmail.com>
2023-06-18 22:38:12 +0600

    start the about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,6 +3,8 @@ import Stats from '../../components/Stats';
 import Info from '../../components/Info';
 import {FaDownload} from 'react-icons/fa';
 import CV from '../../assets/steve-Cv.pdf';
+import './About.css';
+import Skills from '../../components/Skills';
 
 
 const About = () => {
@@ -25,6 +27,20 @@ const About = () => {
                             Download CV <span className='button__icon'><FaDownload className='mt-5 ml-5'  /> </span>
                         </a>
                     </div>
+                    <div className='stats grid'>
+                        <Stats />
+                    </div>
+                </div>
+            </section>
+
+            <div className='separator'></div>
+            
+
+            <section className='skills'>
+                <h3 className='section__subtitle subtitle__center'>My Skills</h3>
+
+                <div className='skills__container grid'>
+                    <Skills />
                 </div>
             </section>
         </main>
