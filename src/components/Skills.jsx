aafaf469a9fd61d1898a4a55bbcd648550cac078
smshaown <smshaown65@gmail.com>
2023-06-19 07:10:01 +0600

    skills done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { skills } from '../data';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Skills = () => {
+
     return (
-        <div>
-            
-        </div>
+        <>
+            {skills.map(({title, percentage}, index) => {
+                return(
+                    <div className="progress__box" key={index}>
+                        <div className="progress__circle">
+                            <CircularProgressbar strokeWidth={7.5} text={`${percentage}%`} value={percentage} />
+                        </div>
+                        
+                        <h3 className='skills__title mt-2'>{title}</h3>
+                    </div>
+                )
+            })}
+        </>
     );
 };
 
